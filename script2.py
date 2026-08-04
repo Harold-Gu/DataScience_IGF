cf8438d0c3a8d85dae1a200ca40c4d93b4d0112a
Harold_Gu <guhaoran1216@gmail.com>
2026-08-04 18:08:53 +0100

    Upgrading the code logic

--- script2.py
+++ script2.py
@@ -53,7 +53,7 @@ def get_all_target_links(list_url):
             if target_year and f'igf-{target_year}-' in href:
                 is_target = True
 
-            elif any(kw in href for kw in ['workshop', 'open-forum', 'lightning', 'session','network','day','pre-event','launches','awards']):
+            elif any(kw in href for kw in ['workshop', 'open-forum', 'lightning', 'session','network','day','pre-event','launches','awards',]):
                 is_target = True
 
             if is_target:
@@ -125,6 +125,6 @@ if __name__ == "__main__":
 
             time.sleep(random.uniform(1.0, 2.5))
 
-        print(f"\n🎉 success{run_save_dir}")
+        print(f"\n success{run_save_dir}")
     else:
         print("\nerror")
