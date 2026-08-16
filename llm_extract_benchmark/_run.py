f2c788702840429ceaff4a337441447d66b53993
Harold_Gu <guhaoran1216@gmail.com>
2026-08-16 23:43:09 +0100

    The entire project structure was upgraded; a verification test step for extracting keywords from LLM was added;

--- llm_extract_benchmark/_run.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-sys.path.insert(0, r'C:\Users\guhao\PyCharmMiscProject\llm_extract_benchmark')
-script = sys.argv[1]
-sys.argv = [script] + sys.argv[2:]
-exec(open(script, encoding='utf-8-sig').read())
